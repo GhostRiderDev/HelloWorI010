ce1a09ff755a851a09af4f1bf1b80869e207d57a
cos4h <olvadis2004@gmail.com>
2024-12-04 21:15:59 -0500

    hello

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,167 @@
+const Footer = () => {
+  return (
+    <footer className="bg-black text-white border-t-2 border-red-700 shadow-lg">
+      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <div className="md:flex md:justify-between">
+          <div className="mb-6 md:mb-0">
+            <a href="/" className="flex items-center">
+              <img
+                src="logo-with-name.png"
+                className="h-8 me-3"
+                alt="Power Training logo"
+              />
+              <span className="self-center text-2xl font-semibold whitespace-nowrap text-red-600">
+                Power Training
+              </span>
+            </a>
+          </div>
+          <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+            <div>
+              <h2 className="mb-6 text-sm font-semibold text-gray-300 uppercase">
+                Recursos
+              </h2>
+              <ul className="text-gray-500 font-medium">
+                <li className="mb-4">
+                  <a href="/" className="hover:underline">
+                    Power Training™
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://tailwindcss.com/"
+                    className="hover:underline"
+                  >
+                    Tailwind CSS
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div>
+              <h2 className="mb-6 text-sm font-semibold text-gray-300 uppercase">
+                Legal
+              </h2>
+              <ul className="text-gray-500 font-medium">
+                <li className="mb-4">
+                  <a href="#" className="hover:underline">
+                    Politica de privacidad
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:underline">
+                    Terminos &amp; Condiciones
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center">
+            © 2024{" "}
+            <a href="/" className="hover:underline">
+              Power Training™
+            </a>
+            . Todos los derechos reservados.
+          </span>
+          <div className="flex mt-4 sm:justify-center sm:mt-0">
+            <a
+              href="#"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+            >
+              <svg
+                className="w-4 h-4"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 8 19"
+              >
+
+              </svg>
+            </a>
+            <a
+              href="#"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+            >
+              <svg
+                className="w-4 h-4"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 21 16"
+              >
+
+              </svg>
+              <span className="sr-only">Discord community</span>
+            </a>
+            <a
+              href="#"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+            >
+              <svg
+                className="w-4 h-4"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 17"
+              >
+
+              </svg>
+              <span className="sr-only">Twitter page</span>
+            </a>
+            <a
+              href="#"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+            >
+              <svg
+                className="w-4 h-4"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+
+              </svg>
+              <span className="sr-only">Facebook page</span>
+            </a>
+            <a
+              href="#"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+            >
+              <svg
+                className="w-4 h-4"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+
+              </svg>
+              <span className="sr-only">Instagram page</span>
+            </a>
+            <a
+              href="#"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+            >
+              <svg
+                className="w-4 h-4"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+
+              </svg>
+              <span className="sr-only">LinkedIn page</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
+
+
